995786de14f6bb8c824a0695cdb0943bdb6e5dd8
k1skac <110919738+k1skac@users.noreply.github.com>
2024-03-16 18:44:20 +0100

    add new app followers to the project, and profile Detailview

--- til/urls.py
+++ til/urls.py
@@ -19,10 +19,12 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from feed import urls as feed_urls
 from django.conf import settings
+from profiles import urls as profile_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(feed_urls, namespace='feed')),
+    path('profile/', include(profile_urls, namespace='profile')),
     re_path("", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
